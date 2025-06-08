e0b64e8ba8d933749a07dd38cd8e99281fa5b495
Md Masud karim <msmasud578@gmail.com>
2025-06-08 16:15:06 +0200

    Added Class named DocumentHandler to the source code

---
+++ app/src/main/java/com/mksoftwaresolutions/image_to_pdf_converter/services/DocumentHandler.kt
@@ -0,0 +1,19 @@
+package com.mksoftwaresolutions.image_to_pdf_converter.services
+
+import java.io.File
+/**
+ * <h1>DocumentHandler</h1>
+ * Document handler handles creation, edition and deletion of a pdf document. It also fetches a list of pdf documents that\
+ * has been created by the app only. Means it will put all the pdf file inside a folder called pdf_converter_document; By this
+ * It knows where to find for pdf files so that other unnecessary pdfs doesn't get included.
+ * */
+class DocumentHandler {
+    val CREATE_FILE = 1
+
+    fun createFile(fileData: File) {
+
+    }
+    fun createFile(data:String) {
+
+    }
+}
