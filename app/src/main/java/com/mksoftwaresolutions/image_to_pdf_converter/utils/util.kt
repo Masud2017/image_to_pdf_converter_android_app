1f6974719415f5f17be8a8bf0121aaa47afada2e
Md Masud karim <msmasud578@gmail.com>
2025-07-27 00:34:25 +0200

    Removed the utils folder and it's files

--- app/src/main/java/com/mksoftwaresolutions/image_to_pdf_converter/utils/util.kt
+++
@@ -1,8 +0,0 @@
-package com.mksoftwaresolutions.image_to_pdf_converter.utils
-
-import android.content.Context
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.rewarded.RewardedAd
-import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback
-
-
